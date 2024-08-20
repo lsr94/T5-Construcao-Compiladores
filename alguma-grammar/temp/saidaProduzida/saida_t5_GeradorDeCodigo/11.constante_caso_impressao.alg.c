a45de4dce802faff954f383bd72949de54b64814
Lara <larasr@estudante.ufscar.br>
2024-08-20 09:41:34 -0300

    Creating the code generator in C

--- alguma-grammar/temp/saidaProduzida/saida_t5_GeradorDeCodigo/11.constante_caso_impressao.alg.c
+++
@@ -1,4 +0,0 @@
-Linha 1: identificador teste ja declarado anteriormente
-Linha 1: identificador teste ja declarado anteriormente
-Linha 1: identificador teste ja declarado anteriormente
-Fim da compilacao
--- alguma-grammar/temp/saidaProduzida/saida_t5_GeradorDeCodigo/19.funcao_dobro_impressao.alg.c
+++
@@ -1,6 +0,0 @@
-Linha 2: comando retorne nao permitido nesse escopo
-Linha 2: comando retorne nao permitido nesse escopo
-Linha 2: comando retorne nao permitido nesse escopo
-Linha 2: comando retorne nao permitido nesse escopo
-Linha 6: incompatibilidade de parametros na chamada de dobro
-Fim da compilacao
